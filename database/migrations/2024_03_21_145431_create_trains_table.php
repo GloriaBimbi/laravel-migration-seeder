3ea622ecc8aa1c988d2a7c49de7526d453d99435
GloriaBimbi <gloria.bimbi1@libero.it>
2024-03-21 16:17:01 +0100

    feat migration

---
+++ database/migrations/2024_03_21_145431_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->dateTime('departure_date');
+            $table->dateTime('arruval_date');
+            $table->string('train_code', 15);
+            $table->tinyInteger('wagons_number')->nullable();
+            $table->boolean('on_time')->devfault(true);
+            $table->boolean('cancelled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
